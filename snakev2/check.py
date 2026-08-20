a82a916d24c0139b86726a955f151e59c3b3f23b
Anant Naithani <anant7738w@gmail.com>
2026-08-20 08:26:07 -0700

    when snake dies the games restarts after one second

--- snakev2/check.py
+++ snakev2/check.py
@@ -1,10 +1,5 @@
-leaderpos = 67
-
-for i in range(10):
-    position  = i
-    print(leaderpos, position)
-    leaderpos = position
-
-numbers = [1, 2, 3, 4,5 ,6 ,7]
-for iterms in numbers[1:]:
-    print(iterms)
+from turtle import Screen, Turtle
+jim = Turtle()
+jim.shape("square")
+screen = Screen()
+screen.textinput("name", "check this is the check")
--- snakev2/main.py
+++ snakev2/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle , Screen 
+from turtle import Turtle , Screen
 from snake import Snake
 from time import sleep
 from random import randint
@@ -7,31 +7,45 @@ screen = Screen()
 screen.setup(600, 600)
 screen.bgcolor("black")
 
-list = []
+#all snake turtle will be inside of this list
+listforsnake = []
 
+#making the leader turtle which will lead the rest of the turtles
 lisan_al_gaib = Snake()
 lisan_al_gaib.color("green")
+
+#turtle who will write the words game over when snake dies
 busted = Turtle()
 busted.ht()
 busted.penup()
 busted.color("white")
+
+"""turtle who will write the score on the screen"""
 scoreturtle = Turtle(visible=False)
+
+"""initial score"""
 score = 0
 
+
+"""turtle for the apple"""
 apple = Turtle()
 apple.teleport(randint(-250,250),randint(-250,250))
 apple.shape("circle")
 apple.color("red")
 apple.penup()
 
-list.append(lisan_al_gaib)
+"""append leader and rest 3 of the snake body"""
+listforsnake.append(lisan_al_gaib)
 for i in range(3):
     body = Snake()
-    list.append(body)
+    listforsnake.append(body)
 
 
-
+#variable to make the game loop running
 running = True
+
+
+"""functions which will be used for the listen command in python"""
 def up():
     if lisan_al_gaib.heading() != 270.00:
         lisan_al_gaib.setheading(90)
@@ -56,31 +70,65 @@ def right():
     else:
         pass
 
+"""we will change this variable to increase the difficulty of the game with time"""
 speed = 0.1
+
 
 screen.listen()
 screen.tracer(0)
+
+"""function which resets the game when pressed r"""
+
+def restart():
+    global score
+    global body
+    score = 0
+
+    for body in listforsnake[4:]:
+        body.hideturtle()
+        listforsnake.remove(body)
+
+    listforsnake[0].setheading(0)
+    listforsnake[0].setpos(0.0,0.0)
+    listforsnake[1].setpos(-20,0.0)
+    listforsnake[2].setpos(-40, 0.0)
+    listforsnake[3].setpos(-60, 0.0)
+
+    busted.reset()
+    busted.ht()
+    busted.penup()
+    busted.color("white")
+
+
+screen.onkey(up, "Up")
+screen.onkey(down, "Down")
+screen.onkey(left, "Left")
+screen.onkey(right, "Right")
+
+
 while running:
+    """changing the difficulty of the game"""
     if score == 10:
         speed = 0.08
     if score == 20:
         speed = 0.05
     if score == 30:
         speed = 0.03
-    screen.onkey(up, "Up")
-    screen.onkey(down, "Down")
-    screen.onkey(left, "Left")
-    screen.onkey(right, "Right")
+
+
+
 
     leader_position_xcor = lisan_al_gaib.xcor()
     leader_position_ycor = lisan_al_gaib.ycor()
     lisan_al_gaib.fd(20)
+
     if lisan_al_gaib.distance(apple) <= 20:
         apple.teleport(randint(-250,250),randint(-250,250))
         score += 1
         body = Snake()
-        list.append(body)
-    for i in list[1:]:
+        listforsnake.append(body)
+
+    for i in listforsnake[1:]:
         positiontempx = i.xcor()
         positiontempy = i.ycor()
 
@@ -91,17 +139,37 @@ while running:
         leader_position_ycor = positiontempy
 
     if lisan_al_gaib.xcor() >= 290:
-        running  = False
-        busted.write("Game Over", False,"center", ("courier", 20, "normal"))
+        busted.write("Game Over", False, "center", ("courier", 20, "normal"))
+        screen.update()
+        sleep(1)
+        restart()
+
+
+
     if lisan_al_gaib.xcor() <= -290:
-        running  = False
-        busted.write("Game Over", False,"center", ("courier", 20, "normal"))
+        busted.write("Game Over", False, "center", ("courier", 20, "normal"))
+        screen.update()
+        sleep(1)
+        restart()
+
+
+
     if lisan_al_gaib.ycor() >= 290:
-        running  = False
-        busted.write("Game Over", False,"center", ("courier", 20, "normal"))
+
+        busted.write("Game Over", False, "center", ("courier", 20, "normal"))
+        screen.update()
+        sleep(1)
+        restart()
+
+
     if lisan_al_gaib.ycor() <= -290:
-        running  = False
-        busted.write("Game Over", False,"center", ("courier", 20, "normal"))
+        busted.write("Game Over", False, "center", ("courier", 20, "normal"))
+        screen.update()
+        sleep(1)
+        restart()
+
+
+
     scoreturtle.reset()
     scoreturtle.ht()
     scoreturtle.color("white")
